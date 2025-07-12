f3969a9f611c286cdf3ab751b6a9d3ac069336c7
hrbtth <42539808+hrbtth@users.noreply.github.com>
2025-07-12 16:07:21 +0530

    base fixed

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: Ao2rqke2 */
+/* Manifest version: HBJCY88j */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
